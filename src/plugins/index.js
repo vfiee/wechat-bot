5a743e33f2f410fe8901fd90deaa52f4124fa1c1
vyron <fiee_vyron@163.com>
2021-10-21 09:24:51 +0800

    feat: 移除额外的 js 文件

--- src/plugins/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var weather_1 = require("./weather");
-var command_1 = require("./command");
-exports["default"] = [weather_1["default"], command_1["default"]];
