5b3c79f821eb0117ff594ef707be6bfa213813f3
vyron <fiee_vyron@163.com>
2021-10-25 10:01:35 +0800

    type: 调整 ts 类型

--- src/plugins/command/index.ts
+++ src/plugins/command/index.ts
@@ -1,57 +1,58 @@
 /*
  * @Author: vyron
  * @Date: 2021-08-14 22:51:42
- * @LastEditTime: 2021-10-05 18:33:14
+ * @LastEditTime: 2021-10-25 09:54:12
  * @LastEditors: vyron
  * @Description: Bot command plugin
  * @FilePath: /wechat-bot/src/plugins/command/index.ts
  */
 // @ts-ignore
-import {config, Message, Room, Wechaty} from "/wechaty";
-import {isOwner, BLANK} from "../../utils";
-import {sendWeatherMessage} from "../weather";
-import configs, {WeatherConfig} from "../weather/config";
+import { Message, Wechaty } from "/wechaty"
+import { BLANK } from "../../utils"
+import { sendWeatherMessage } from "../weather"
+import configs, { WeatherConfig } from "../weather/config"
 
 enum Commands {
-	WEATHER = "@天气",
-	ROOM_KICK = "@踢"
+  WEATHER = "@天气",
+  ROOM_KICK = "@踢",
 }
 
-type CommandFunction = (msg: Message, command: string) => void;
+type CommandFunction = (msg: Message, command: string) => void
 
 type CommandMaps = {
-	[key: string]: CommandFunction;
-};
+  [key: string]: CommandFunction
+}
 
 const runServiceByCommand = async (command: string, msg: Message) => {
-	const commands: CommandMaps = {
-		[Commands.WEATHER]: weather,
-		[Commands.ROOM_KICK]: BLANK
-	};
-	const fn = commands[command];
-	fn && typeof fn === "function" && fn(msg, command as Commands);
-};
+  const commands: CommandMaps = {
+    [Commands.WEATHER]: weather,
+    [Commands.ROOM_KICK]: BLANK,
+  }
+  const fn = commands[command]
+  fn && typeof fn === "function" && fn(msg, command as Commands)
+}
 
 const command = (bot: Wechaty) => {
-	bot.on("message", async (msg: Message) => {
-		if (msg.self()) return;
-		const text = await msg.mentionText();
-		const isCommand = Object.values(Commands).includes(text as Commands);
-		isCommand && runServiceByCommand(text, msg);
-	});
-};
+  bot.on("message", async (msg: Message) => {
+    if (msg.self()) return
+    const text = await msg.mentionText()
+    const isCommand = Object.values(Commands).includes(text as Commands)
+    isCommand && runServiceByCommand(text, msg)
+  })
+}
 
 // 发送天气情况
 const weather = async (msg: Message, command: string) => {
-	const talkerName = msg.talker().name();
-	const talkerConfigIndex = configs.findIndex(
-		config => config.contactName === talkerName
-	);
-	if (talkerConfigIndex === -1) {
-		msg.say(`@${talkerName} 您尚未配置天气需要的经纬度信息,请联系管理员配置!`);
-		return;
-	}
-	await sendWeatherMessage(configs[talkerConfigIndex], msg);
-};
+  console.log(`command:`, command)
+  const talkerName = msg.talker().name()
+  const talkerConfigIndex = configs.findIndex(
+    (config) => config.contactName === talkerName
+  )
+  if (talkerConfigIndex === -1) {
+    msg.say(`@${talkerName} 您尚未配置天气需要的经纬度信息,请联系管理员配置!`)
+    return
+  }
+  await sendWeatherMessage(configs[talkerConfigIndex] as WeatherConfig, msg)
+}
 
-export default command;
+export default command
--- src/plugins/weather/config.ts
+++ src/plugins/weather/config.ts
@@ -1,156 +1,156 @@
 /*
  * @Author: vyron
  * @Date: 2021-08-15 00:06:09
- * @LastEditTime: 2021-10-05 19:33:26
+ * @LastEditTime: 2021-10-25 09:51:54
  * @LastEditors: vyron
  * @Description: 天气配置文件
  * @FilePath: /wechat-bot/src/plugins/weather/config.ts
  */
 export interface WeatherTemplateData {
-	temperature: number;
-	minTemperature: number;
-	maxTemperature: number;
-	minWind: number;
-	maxWind: number;
-	windDirection: string;
-	isDay: boolean;
-	isNight: boolean;
-	date: string;
-	weather: string;
-	address: string;
+  temperature: number
+  minTemperature: number
+  maxTemperature: number
+  minWind: number
+  maxWind: number
+  windDirection: string
+  isDay: boolean
+  isNight: boolean
+  date: string
+  weather: string
+  address: string
 }
 
-export type WeatherMessageTemplate = (data: WeatherTemplateData) => string;
+export type WeatherMessageTemplate = (data: WeatherTemplateData) => string
 
 export type WeatherPosition = {
-	longitude: number;
-	latitude: number;
-};
+  longitude: number
+  latitude: number
+}
 
-type TimeMap = [number[], string];
+type TimeMap = [[number, number], string]
 
 const getGreetByTime = () => {
-	const hours = Number(new Date().getHours());
-	const timeMaps: TimeMap[] = [
-		[[5, 6], "清晨"],
-		[[7, 11], "早上"],
-		[[11, 13], "中午"],
-		[[12, 18], "下午"],
-		[[18, 24], "晚上"],
-		[[24, 5], "凌晨"]
-	];
-	let res = "";
-	for (const [time, greet] of timeMaps) {
-		const [min, max] = time;
-		if (hours > min && hours <= max) {
-			res = greet as string;
-			break;
-		}
-	}
-	return res;
-};
+  const hours = Number(new Date().getHours())
+  const timeMaps: TimeMap[] = [
+    [[5, 6], "清晨"],
+    [[7, 11], "早上"],
+    [[11, 13], "中午"],
+    [[12, 18], "下午"],
+    [[18, 24], "晚上"],
+    [[24, 5], "凌晨"],
+  ]
+  let res = ""
+  for (const [time, greet] of timeMaps) {
+    const [min, max] = time
+    if (hours > min && hours <= max) {
+      res = greet as string
+      break
+    }
+  }
+  return res
+}
 
 export interface WeatherConfig {
-	contactId: string;
-	contactName: string;
-	scheduler: string;
-	prefix?: string;
-	suffix?: string;
-	disabled?: boolean;
-	template: WeatherMessageTemplate;
-	position: WeatherPosition;
+  contactId: string
+  contactName: string
+  scheduler: string
+  prefix?: string
+  suffix?: string
+  disabled?: boolean
+  template: WeatherMessageTemplate
+  position: WeatherPosition
 }
 
 export function defaultWeatherMessageTemplate(
-	this: WeatherConfig,
-	data: WeatherTemplateData
+  this: WeatherConfig,
+  data: WeatherTemplateData
 ) {
-	let {
-		minTemperature,
-		maxTemperature,
-		minWind,
-		maxWind,
-		windDirection,
-		isNight,
-		date,
-		weather,
-		temperature,
-		address
-	} = data || {};
-	if (!data || Object.keys(data).length === 0) return "";
-	const prefix = this.prefix;
-	const suffix = this.suffix;
-	return `${prefix ? prefix + "\n\n" : ""}${date || ""}${
-		isNight ? "夜" : "日"
-	} ${weather || ""}
+  let {
+    minTemperature,
+    maxTemperature,
+    minWind,
+    maxWind,
+    windDirection,
+    isNight,
+    date,
+    weather,
+    temperature,
+    address,
+  } = data || {}
+  if (!data || Object.keys(data).length === 0) return ""
+  const prefix = this.prefix
+  const suffix = this.suffix
+  return `${prefix ? prefix + "\n\n" : ""}${date || ""}${
+    isNight ? "夜" : "日"
+  } ${weather || ""}
 位置: ${address || ""}
 温度: ${temperature || ""}℃ ${minTemperature || 0}-${maxTemperature || 0}℃
 风况: ${windDirection || ""} ${minWind || 0}级-${maxWind || 0}级${
-		suffix ? "\n\n" + suffix : ""
-	}`;
+    suffix ? "\n\n" + suffix : ""
+  }`
 }
 
 const config: WeatherConfig[] = [
-	{
-		contactId: "xiaohui-up",
-		contactName: "A小慧",
-		scheduler: "0 0 8 * * *",
-		prefix: "亲爱的宝贝~ ",
-		suffix: "新的一天,新的开始,保持好心情,继续加油! 奥利给💋💋💋",
-		template: defaultWeatherMessageTemplate,
-		position: {
-			longitude: 113.544109,
-			latitude: 34.806363
-		}
-	},
-	{
-		contactId: "姜先生",
-		contactName: "vyron",
-		scheduler: "0 0 8 * * *",
-		prefix: `主人${getGreetByTime()}好~`,
-		suffix: "祝主人整天心情愉快 🥰🥰🥰",
-		template: defaultWeatherMessageTemplate,
-		position: {
-			longitude: 113.544109,
-			latitude: 34.806363
-		}
-	},
-	{
-		contactId: "fuyaohui66",
-		contactName: "Hope Of The Whole Village",
-		scheduler: "0 0 8 * * *",
-		prefix: `小灰灰同学${getGreetByTime()}好~`,
-		suffix: "新的一天,新的开始,保持好心情,继续加油码代码! 奥利给👍👍👍",
-		template: defaultWeatherMessageTemplate,
-		position: {
-			longitude: 114.757824,
-			latitude: 32.830579
-		}
-	},
-	{
-		contactId: "Flipped_Zyy",
-		contactName: "张洋洋🐑",
-		scheduler: "0 0 8 * * *",
-		prefix: `张洋洋同学${getGreetByTime()}好~`,
-		suffix: "新的一天,新的开始,保持好心情,继续加油! 奥利给👍👍👍",
-		template: defaultWeatherMessageTemplate,
-		position: {
-			longitude: 116.304423,
-			latitude: 39.963
-		}
-	},
-	{
-		contactId: "rypbklsx0726",
-		contactName: "十年",
-		scheduler: "0 0 8 * * *",
-		prefix: `杨凯同学${getGreetByTime()}好~`,
-		suffix: "新的一天,新的开始,保持好心情,继续加油码代码! 奥利给👍👍👍",
-		template: defaultWeatherMessageTemplate,
-		position: {
-			longitude: 114.102989,
-			latitude: 32.084698
-		}
-	}
-];
+  {
+    contactId: "xiaohui-up",
+    contactName: "A小慧",
+    scheduler: "0 0 8 * * *",
+    prefix: "亲爱的宝贝~ ",
+    suffix: "新的一天,新的开始,保持好心情,继续加油! 奥利给💋💋💋",
+    template: defaultWeatherMessageTemplate,
+    position: {
+      longitude: 113.544109,
+      latitude: 34.806363,
+    },
+  },
+  {
+    contactId: "姜先生",
+    contactName: "vyron",
+    scheduler: "0 0 8 * * *",
+    prefix: `主人${getGreetByTime()}好~`,
+    suffix: "祝主人整天心情愉快 🥰🥰🥰",
+    template: defaultWeatherMessageTemplate,
+    position: {
+      longitude: 113.544109,
+      latitude: 34.806363,
+    },
+  },
+  {
+    contactId: "fuyaohui66",
+    contactName: "Hope Of The Whole Village",
+    scheduler: "0 0 8 * * *",
+    prefix: `小灰灰同学${getGreetByTime()}好~`,
+    suffix: "新的一天,新的开始,保持好心情,继续加油码代码! 奥利给👍👍👍",
+    template: defaultWeatherMessageTemplate,
+    position: {
+      longitude: 114.757824,
+      latitude: 32.830579,
+    },
+  },
+  {
+    contactId: "Flipped_Zyy",
+    contactName: "张洋洋🐑",
+    scheduler: "0 0 8 * * *",
+    prefix: `张洋洋同学${getGreetByTime()}好~`,
+    suffix: "新的一天,新的开始,保持好心情,继续加油! 奥利给👍👍👍",
+    template: defaultWeatherMessageTemplate,
+    position: {
+      longitude: 116.304423,
+      latitude: 39.963,
+    },
+  },
+  {
+    contactId: "rypbklsx0726",
+    contactName: "十年",
+    scheduler: "0 0 8 * * *",
+    prefix: `杨凯同学${getGreetByTime()}好~`,
+    suffix: "新的一天,新的开始,保持好心情,继续加油码代码! 奥利给👍👍👍",
+    template: defaultWeatherMessageTemplate,
+    position: {
+      longitude: 114.102989,
+      latitude: 32.084698,
+    },
+  },
+]
 
-export default config;
+export default config
